85adf3874528a80a0ff4bc491ca2d37ea87da713
Vosik <vosikvos@gmail.com>
2025-02-22 21:39:14 +0200

    F - setup TabView

---
+++ InstagramClone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,45 @@
+//
+//  MainTabView.swift
+//  InstagramClone
+//
+//  Created by Саша Восколович on 22.02.2025.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    
+    enum TabIdentifier: Hashable {
+        case feed, profile, search, uploadPosts, notifications
+    }
+
+    @State private var selectedTab: TabIdentifier = .feed
+    
+    var body: some View {
+        
+        TabView(selection: $selectedTab) {
+            
+            
+            Tab("", systemImage: "house", value: .feed) {
+                
+            }
+            Tab("", systemImage: "magnifyingglass", value: .search) {
+                
+            }
+            Tab("", systemImage: "plus.square", value: .uploadPosts) {
+                
+            }
+            Tab("", systemImage: "heart", value: .notifications) {
+                
+            }
+            Tab("", systemImage: "person", value: .profile) {
+                
+            }
+        }
+        .accentColor(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
